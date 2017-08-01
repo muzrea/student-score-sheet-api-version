7028af4c66d7b6210a908569475efb1b02abbfbe
Muzrea <mu@hotmail.com>
2017-08-01 09:20:11 +0800

    first commit

--- period3.js
+++ period3.js
@@ -14,51 +14,77 @@ var client = redis.createClient('6379', '127.0.0.1');
 client.on("error", function(error) {
     console.log(error);
 });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
 app.use(express.static('public'));
 app.get('/index.html', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
 })
 var server = app.listen(8081, function () {
-
     var host = server.address().address
     var port = server.address().port
-
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
+});
 
+//
+app.get('/student', urlencodedParser, function (req, res){
+    let items=[];
+    let stu=req.body.sInfo;
+    items.push(stu);
+    client.get("students", function(err, value) {
+        if(value){
+            let students=JSON.parse(value);
+            students.push(stu);
+            client.set("students",JSON.stringify(students),function (err,reply) {
+                //console.log(reply);
+            })
+        }else {
+            client.set("students",JSON.stringify(items),function (err,reply) {
+                //console.log(reply);
+            })
+        }
+        res.send(students);
+    });
+});
 
-})
-app.post('/student', urlencodedParser, function (req, res) {
-    // 输出 JSON 格式
-    res.set('Content-Type','text/javascript;charset=UTF-8');
-    client.set('sInfos', req.body.sInfo); //新增
-    var response = {
-        "sInfos":req.body.sInfo
-    };
-    // var s = client.getAttribute('sInfos');
-    console.log(response);
-    res.send(s);
-    res.end(JSON.stringify(response));
-})
-
-//查询
+//查询得到要查询的学生学号
 app.post('/students', urlencodedParser, function (req, res) {
     // 输出 JSON 格式
     res.set('Content-Type','text/javascript;charset=UTF-8');
     client.set('snos', req.body.items);
-    var response = {
-        "sno":req.body.items
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+    client.get("snos",function(err,val){
+        res.send(val);
+    });
 })
 
-// app.get('/students', urlencodedParser, function (req, res){
-//     res.set('Content-Type','text/javascript;charset=UTF-8');
-//     client.get('sInfos', function(err, reply) { //查詢
-//         res.send(reply);//輸出 countReq
-//     });
-// })
-
+function getStuInfo(){
+    let inputSno = document.getElementById("items").value;
+    inputSno.split('，');
+    for(let i in inputSno){
+        document.getElementById('myTable').deleteRow(i+1);
+    }
+    let allInfo = showStu();
+    let requiredItems = {};
+    for(let i in inputSno){
+        for(let j in allInfo){
+            if(inputSno[i]==allInfo[j].snos){
+                requiredItems.push(allInfo[j]);
+            }
+        }
+    }
+    //在页面显示
+    let getRequiredItems = ' ';
+    for(let i=0;i<inputSno.length;i++){
+        getRequiredItems += "<tr class='active'>" +"<td>" +` ${getRequiredItems[i].name}`+"</td>"
+            +"<td>" +` ${getRequiredItems[i].math}`+"</td>"
+            +"<td>" +` ${getRequiredItems[i].chinese}`+"</td>"
+            +"<td>" +` ${getRequiredItems[i].english}`+"</td>"
+            +"<td>" +` ${getRequiredItems[i].code}`+"</td>"
+            +"<td><input type='button' value='修改' ></input></td>"
+            +"<td><input type='button' value='删除' ></td>"+"</tr>";
+    }
+    $("#myTable").append(getRequiredItems);
+}
 
 
 
